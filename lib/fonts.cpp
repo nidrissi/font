79b8b7099f457f087ddc60099d1649210b2552cb
Najib Idrissi-Kaïtouni <najib.idrissi.kaitouni@outlook.com>
2024-01-31 17:17:26 +0100

    bound check

--- lib/fonts.cpp
+++ lib/fonts.cpp
@@ -2,6 +2,9 @@
 
 bool monofont::Font::operator()(const char &c, const size_t &x,
                                 const size_t &y) const {
+  if (x >= width || y >= height) {
+    throw std::out_of_range("Font::operator(): out of range");
+  }
   auto start_position = positions.at(c);
   auto position = start_position + (y * bytes_per_char) + (x / 8);
   uint8_t offset = x % 8;
