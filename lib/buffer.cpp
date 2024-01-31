5d3a573bc5201c703ab7ddd013e7c80779097072
Najib Idrissi-Kaïtouni <najib.idrissi.kaitouni@outlook.com>
2024-01-31 11:51:23 +0100

    basic matrix

--- lib/buffer.cpp
+++ lib/buffer.cpp
@@ -1,2 +1,21 @@
 #include "buffer.hpp"
 #include "fonts.hpp"
+
+monofont::buffer::buffer(const size_t &width, const size_t &height)
+    : width{width}, height{height},
+      data{std::make_unique<bool[]>(width * height)} {}
+
+bool &monofont::buffer::operator()(const size_t &x, const size_t &y) {
+  if (x >= width || y >= height) {
+    throw std::out_of_range("buffer::operator(): out of range");
+  }
+  return data[y * width + x];
+}
+
+const bool &monofont::buffer::operator()(const size_t &x,
+                                         const size_t &y) const {
+  if (x >= width || y >= height) {
+    throw std::out_of_range("buffer::operator(): out of range");
+  }
+  return data[y * width + x];
+}
--- lib/include/buffer.hpp
+++ lib/include/buffer.hpp
@@ -1,11 +1,19 @@
 #pragma once
 #include <cstddef>
+#include <memory>
 
 namespace monofont {
 class buffer {
+public:
   const size_t width;
   const size_t height;
-  buffer(const size_t &width, const size_t &height)
-      : width(width), height(height) {}
+
+  buffer(const size_t &width, const size_t &height);
+
+  bool &operator()(const size_t &x, const size_t &y);
+  const bool &operator()(const size_t &x, const size_t &y) const;
+
+private:
+  std::unique_ptr<bool[]> data;
 };
 }; // namespace monofont
